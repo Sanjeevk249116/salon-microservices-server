2c348d60832e74ce0e1d45ffe561f460af2879a8
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2026-03-18 15:59:09 +0530

    update chnage of setup

---
+++ salon.server/src/main/java/com/salon/salon/server/controller/SalonController.java
@@ -0,0 +1,12 @@
+package com.salon.salon.server.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/salon")
+public class SalonController {
+
+}
